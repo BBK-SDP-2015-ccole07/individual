fd0503eb07b96abb693c0c6045be2f8f17093fa9
Peter Coles <peter@kuroiweb.com>
2015-02-15 21:53:44 +0000

    trap with division by zero errors

--- SML/src/sml/DivInstruction.java
+++ SML/src/sml/DivInstruction.java
@@ -27,7 +27,11 @@ public class DivInstruction extends Instruction {
 	public void execute(Machine m) {
 		int value1 = m.getRegisters().getRegister(op1);
 		int value2 = m.getRegisters().getRegister(op2);
-		m.getRegisters().setRegister(result, value1 / value2);
+		try {
+			m.getRegisters().setRegister(result, value1 / value2);
+		} catch (ArithmeticException e) {
+		     System.out.println("Runtime Error: Division by zero");
+		}
 	}
 
 	@Override
